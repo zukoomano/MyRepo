83623d90100b8c6a888f7407c42f28307896d673
zukoomano <69848074+zukoomano@users.noreply.github.com>
2020-08-18 18:22:11 +0530

    Create Jenkins.sh

---
+++ Jenkins.sh
@@ -0,0 +1,9 @@
+echo "Hi... This is my new Jenkins File !!!"
+echo "====================================="
+echo "Printing the Users Information"
+echo " "
+cat /etc/passwd
+echo " "
+echo "====================================="
+echo " "
+echo "End of the Script"
