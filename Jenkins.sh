6c107e1afad0e6d3b60a8281880c8d5b5ce9f939
zukoomano <69848074+zukoomano@users.noreply.github.com>
2020-08-21 17:42:21 +0530

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -2,20 +2,4 @@ echo "Hi.. My First GIT Jenkins File"
 
 echo "Pipeline  !!"
 
-pipeline {
-    agent { label 'Jenkins2' }
-    
-    stages {
-        stage ( 'Fist Pipeline Project ') {
-            agent { label 'Jenkins2' }
-            
-            steps {
-            echo " Hello All !!!"
-            echo " This is first pipeline Project"
-        
-        }
-    }
-        }
-        
-        
-}
+
