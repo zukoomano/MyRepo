d7d3bae1bdf15e5f1b07d69aa429d00e02e7c6f9
zukoomano <69848074+zukoomano@users.noreply.github.com>
2020-08-21 15:24:44 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,7 +1,7 @@
 yum install wget -y
 wget https://mirrors.estointernet.in/apache/[...]/ApacheDirectoryStudio-2.0.0.v20200411-M15-linux.gtk.x86_64.tar.gz
-tar -xvzf apache-tomcat-8.5.50.tar.gz
-cd apache-tomcat-8.5.50/bin/
+tar -xvzf ApacheDirectoryStudio-2.0.0.v20200411-M15-linux.gtk.x86_64.tar.gz
+cd Apache*
 chmod +x shutdown.sh
 chmod +x startup.sh
 ./startup.sh 
